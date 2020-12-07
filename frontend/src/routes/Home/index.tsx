04e9453d1ce20db300833fbfa5a67f0860ffb543
hui444 <ygreek@gmail.com>
2020-12-07 17:48:13 +0800

    Link buttons in login and signup page

--- frontend/src/routes/Home/index.tsx
+++ frontend/src/routes/Home/index.tsx
@@ -4,12 +4,24 @@ import styled from 'styled-components'
 import TopNavBar from '../../components/Mobile/TopNavBar'
 import { getUpdateMockString } from '../../store/home/action'
 import { RootState } from '../../store/types'
+import NavBar from '../../components/NavBar'
+import { Button } from 'antd'
+import { useRouteMatch, Link } from 'react-router-dom'
+import { PATHS } from '../Routes'
 
 const MainContainer = styled.div`
   height: 100vh;
   width: 100vw;
   background-color: #fafaf4;
 `
+
+const HomeContainer = styled.div`
+  height: 100%;
+  width: 50%;
+  background-color: #f2f7f7;
+  margin: 0 auto;
+`
+
 export default function Home() {
   const dispatch = useDispatch()
   const { sampleStateText } = useSelector((state: RootState) => state.home)
@@ -28,6 +40,14 @@ export default function Home() {
     <MainContainer>
       <TopNavBar title={'NavBarTitle'} leftIconComponent={leftIcon} />
       <button onClick={onButtonClick}>{sampleStateText}</button>
+      <HomeContainer>
+        <Button type="primary" block>
+          <Link to={PATHS.SIGNUP_PAGE}>Sign Up </Link>
+        </Button>
+        <Button type="primary" block>
+          <Link to={PATHS.LOGIN_PAGE}>Login </Link>
+        </Button>
+      </HomeContainer>
     </MainContainer>
   )
 }
